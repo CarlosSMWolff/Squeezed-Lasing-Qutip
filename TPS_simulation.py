c18536652d68dfc86f9608c955150803801525d1
Carlos Sánchez Muñoz <carlossmwolff@gmail.com>
2022-09-28 07:31:46 +0200

    Added steady state function

--- TPS_simulation.py
+++ TPS_simulation.py
@@ -22,13 +22,17 @@ class simulation:
         s = self.s; sigma = self.sigma; sigma1 = self.sigma1; sigma2 = self.sigma2; 
         return [sqrt(kappa)*s, sqrt(P)*sigma.dag(),sqrt(Gamma)*sigma1,sqrt(Gamma)*sigma2 ]
 
-    def g2g1(self,DeltaPair,g,gsens,kappa,P,Gamma,r):
+    def rhoSS(self,DeltaPair,g,gsens,kappa,P,Gamma,r):
         Delta1 = DeltaPair[0]
         Delta2 = DeltaPair[1]
         H = self.gen_H(g,gsens,Delta1,Delta2,r)
         Lops = self.gen_Lops(kappa,P,Gamma)
 
         rho_ss = steadystate(H,Lops,method='iterative-lgmres')
+        return rho_ss
+
+    def g2g1(self,DeltaPair,g,gsens,kappa,P,Gamma,r):
+        rho_ss = self.rhoSS(self,DeltaPair,g,gsens,kappa,P,Gamma,r)
 
         sigma1 = self.sigma1; sigma2 = self.sigma2; 
         G2w1w2 = expect(sigma1.dag()*sigma2.dag()*sigma1*sigma2,rho_ss)
